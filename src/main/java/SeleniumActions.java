78dfb285eed907cbe03b99ad7e13311434beb01f
Tsvetomir Banovski <cvetomir.banovski@mycomplianceoffice.com>
2024-09-28 17:50:33 +0300

    adding folders

--- src/main/java/SeleniumActions.java
+++ src/main/java/SeleniumActions.java
@@ -9,7 +9,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.List;
 import java.util.Set;
 
 public class SeleniumActions {
