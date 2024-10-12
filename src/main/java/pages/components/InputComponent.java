29275e86134a2e67ea76a90ae0556ca02e9f7aa7
Tsvetomir Banovski <cvetomir.banovski@mycomplianceoffice.com>
2024-10-13 00:58:44 +0300

    refactor: Simplify InputComponent and add scrolling functionality

--- src/main/java/pages/components/InputComponent.java
+++ src/main/java/pages/components/InputComponent.java
@@ -2,31 +2,28 @@ package pages.components;
 
 import base.DriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class InputComponent {
-    //    public static WebElement scrollElementToCenter(By by) {
-    //        WebElement element = element(by);
-    //        return scrollElementToCenter(element);
-    //    }
-    //    public static WebElement scrollToLabel(String label) {
-    //        By labelBy = By.xpath("//label[normalize-space() ='"+label+"'] | //label//*[text() ='"+label+"']");
-    //        return scrollElementToCenter(labelBy);
-    //    }
-    //    public static WebElement scrollElementToCenter(WebElement webElement) {
-    //        javaScriptExecutor().executeScript("arguments[0].scrollIntoView({behavior: 'auto', block: 'center', inline: 'center'});", webElement);
-    //        return webElement;
-    //    }
+
+    static JavascriptExecutor javascriptExecutor;
 
     private static WebDriver driver = DriverManager.getDriver();
 
     public static void sendKeys(String labelName, String value) {
-        getInputComponent(labelName).sendKeys(value);
+        getComponent(labelName).sendKeys(value);
     }
 
-    private static WebElement getInputComponent(String labelName) {
+    private static WebElement getComponent(String labelName) {
         return driver.findElement(By.xpath(String.format("//label[text()='%s']/following::input[1]", labelName)));
+    }
+
+    private static void scrollTo(String labelName) {
+        javascriptExecutor.executeScript(
+            "arguments[0].scrollIntoView({behavior: 'auto', block: 'center', inline: 'center'});",
+            getComponent(labelName));
     }
 
 }
